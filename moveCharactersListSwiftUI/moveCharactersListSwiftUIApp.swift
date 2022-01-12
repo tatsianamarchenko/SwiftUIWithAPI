72f4a4e2de8443f2b58fa8c28f13ec3f86db3b32
tatsiana <tanya3308225@mail.ru>
2022-01-12 20:17:53 +0300

    Initial Commit

---
+++ moveCharactersListSwiftUI/moveCharactersListSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  moveCharactersListSwiftUIApp.swift
+//  moveCharactersListSwiftUI
+//
+//  Created by Tatsiana Marchanka on 12.01.22.
+//
+
+import SwiftUI
+
+@main
+struct moveCharactersListSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
